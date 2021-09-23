9f142f7b5a8c06dfc32241bbc9ed271e19397fc5
Michael Kevin Montolalu <montolalumichael@gmail.com>
2021-09-23 23:19:30 +0700

    terekonsiliasi done
    tereknsoliasi done

--- pages/api/kasbank/ubahstatus.js
+++ pages/api/kasbank/ubahstatus.js
@@ -3,42 +3,75 @@ const prisma = new PrismaClient();
 
 export default async (req, res) => {
   try {
-    req.body &&
-      req.body.map((data) => {
-        let parsed = JSON.parse(data);
-        if (parsed.tipe === "kirimuang") {
-          const kirimuang = await prisma.headerKirimUang.update({
-            where: {
-              id: +parsed.id,
-            },
-            data: {
-              status: "sudah di rekonsilisasi",
-            },
-          });
-        } else if (parsed.tipe === "terimauang") {
-          const terimauang = await prisma.headerTerimaUang.update({
-            where: {
-              id: +parsed.id,
-            },
-            data: {
-              status: "sudah di rekonsilisasi",
-            },
-          });
-        } else {
-          const transferuang = await prisma.transferUang.update({
-            where: {
-              id: +parsed.id,
-            },
-            data: {
-              status: "sudah di rekonsilisasi",
-            },
-          });
-        }
-      });
+    if (!req.body) throw new Error("No body on request");
 
-    res.status(201).json({ message: "USER FOUND!", data: req.body });
+    for (const data of req.body) {
+      let parsed = JSON.parse(data);
+      if (parsed.tipe === "kirimuang") {
+        const kirimuang = await prisma.headerKirimUang.update({
+          where: {
+            id: +parsed.id,
+          },
+          data: {
+            status: "Sudah Terekonsilisasi",
+          },
+        });
+      } else if (parsed.tipe === "terimauang") {
+        const terimauang = await prisma.headerTerimaUang.update({
+          where: {
+            id: +parsed.id,
+          },
+          data: {
+            status: "Sudah Terekonsilisasi",
+          },
+        });
+      } else {
+        const transferuang = await prisma.transferUang.update({
+          where: {
+            id: +parsed.id,
+          },
+          data: {
+            status: "Sudah Terekonsilisasi",
+          },
+        });
+      }
+    }
+    // req.body &&
+    //   req.body.map((data) => {
+    //     let parsed = JSON.parse(data);
+    //     if (parsed.tipe === "kirimuang") {
+    //       const kirimuang = await prisma.headerKirimUang.update({
+    //         where: {
+    //           id: +parsed.id,
+    //         },
+    //         data: {
+    //           status: "Sudah Terekonsilisasi",
+    //         },
+    //       });
+    //     } else if (parsed.tipe === "terimauang") {
+    //       const terimauang = await prisma.headerTerimaUang.update({
+    //         where: {
+    //           id: +parsed.id,
+    //         },
+    //         data: {
+    //           status: "Sudah Terekonsilisasi",
+    //         },
+    //       });
+    //     } else {
+    //       const transferuang = await prisma.transferUang.update({
+    //         where: {
+    //           id: +parsed.id,
+    //         },
+    //         data: {
+    //           status: "Sudah Terekonsilisasi",
+    //         },
+    //       });
+    //     }
+    //   });
+
+    res.status(201).json({ message: "Terkonsiliasi Success!", data: req.body });
   } catch (error) {
-    res.status(400).json({ data: "USER NOT FOUND!", error });
+    res.status(400).json({ data: "Terkonsiliasi Failed!", error });
     console.log(error);
   }
 };
